e0ffa5c893a23a98de9305d516bfff7d73edd82e
kalin1233 <kalin32@bu.edu>
2024-01-31 22:24:00 -0500

    Finished Program will add comments later.

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.activity.ComponentActivity
 import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
+//Recieved help from Hemanshu and Ruihang.
+//Used: https://developer.android.com/reference/com/google/android/material/snackbar/Snackbar
+//To learn more about View binding and Snackbar Methods which was cool and helped me with my arithmatic programn.
 class MainActivity : ComponentActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -15,16 +18,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         binding.trueButton.setOnClickListener{
             Snackbar.make(binding.root, "True", Snackbar.LENGTH_SHORT).show()
-            Log.d("ButtonClicked", "True button clicked")
         }
 
         binding.falseButton.setOnClickListener {
             Snackbar.make(binding.root, "False", Snackbar.LENGTH_SHORT).show()
-            Log.d("ButtonClicked", "False button clicked")
         }
     }
 
